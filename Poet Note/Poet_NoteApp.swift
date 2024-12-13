4ebbe7854ae4a1665b81c62450f8edea56364ac6
Mohammad Taohidun Nur Islam <mislam24@fed.idserve.net>
2024-12-13 16:53:30 +0100

    Initial Commit

---
+++ Poet Note/Poet_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poet_NoteApp.swift
+//  Poet Note
+//
+//  Created by Mohammad Islam on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Poet_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
